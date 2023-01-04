f4a58f107dd450b1f59c80c3e3ff7a0b3812d890
Bruno Bezerra da Silva <121889090+bruno-bsilva@users.noreply.github.com>
2023-01-04 12:34:59 -0300

    add razão entre dois números

---
+++ razao-entre-dois-numeros/Program03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números quaisquer e eu dividirei o maior pelo menor");
+            Console.WriteLine("Digite o primeiro valor");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor");
+            int n2= int.Parse(Console.ReadLine());
+            Console.Clear();
+            if (n1 > n2)
+            {
+                Console.WriteLine("O valor final é " + n1 / n2);
+            }
+             else
+            {
+                Console.WriteLine("O valor final é " + n2 / n1);
+            }
+        }
+
+    }
+}
