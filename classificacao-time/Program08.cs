cbb13222fd09af71d9d731e01203cd45748b0280
Bruno Bezerra da Silva <121889090+bruno-bsilva@users.noreply.github.com>
2023-02-02 14:28:22 -0300

    Add files via upload

---
+++ classificacao-time/Program08.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de vitórias da sua equipe ");
+            int vit = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número de derrotas da sua equipe ");
+            int der = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            if (vit> der)
+            {
+                Console.WriteLine("Pronto para a Libertadores");
+            }
+            else if (vit < der)
+            {
+                Console.WriteLine("Arêrê seu time vai jogar a série B");
+            }
+            else
+            {
+                Console.WriteLine("Pelo menos não vai cair");
+            }
+        }
+
+    }
+}
---
+++ classificacao-time/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("08")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("08")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("785941d7-12bd-490d-8af9-dabc2ebf697f")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
