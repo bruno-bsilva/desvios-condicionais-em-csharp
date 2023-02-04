18eaa76051cd35ebbd319d085069ccf94b470950
Bruno Bezerra da Silva <121889090+bruno-bsilva@users.noreply.github.com>
2023-02-04 11:49:42 -0300

    Add files via upload

---
+++ credito/Program09.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o quanto pretende pagar mensalmente ");
+            int pre =int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu sálario bruto ");
+            int sal =int.Parse(Console.ReadLine());
+
+            if (pre * 0.30 > sal)
+            {
+                Console.WriteLine("Infelizmente seu crédito não foi aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Parabéns, seu crédito foi aprovado");
+            }
+                
+        }
+    }
+}
